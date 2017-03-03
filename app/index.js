43c6012f420b6527c61fbdbdb42d442af2b8d743
Dibyaranjan Gouda <DGouda@ESTUATE.COM>
2017-03-03 18:02:05 +0530

    commiting of  blog

--- app/index.js
+++ app/index.js
@@ -1,1 +1,14 @@
-var application=angular.module('git',[]);
+var application=angular.module('git',['ngRoute']);
+application.config(function($routeProvider)
+{
+    $routeProvider
+        .when('/blog',
+        {
+            // templateUrl:"login/login.html",
+            // controller:"myCtrl"
+			templateUrl:"Blog/blog.html"
+            
+
+        })
+   
+});
