02246bc2dc3cb1e44cd88a5ab406220a397aef4c
Deeptikanta Panda <DPanda@ESTUATE.COM>
2017-03-03 17:16:20 +0530

    login page commit

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-var application=angular.module('git',[]);
+var application=angular.module('git',['ngRoute']);
+application.config(function($routeProvider)
+{
+    $routeProvider
+        .when('/',
+        {
+            templateUrl:"login/login.html",
+            controller:"myCtrl"
+
+        })
+   
+});
