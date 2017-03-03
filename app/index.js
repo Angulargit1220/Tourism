d434e9dd622af092b19a935192b1f312b4957d6d
abhijeet <kumarabhijeet2991@gmail.com>
2017-03-03 17:30:01 +0530

    this is contact commit

--- app/index.js
+++ app/index.js
@@ -1,1 +1,9 @@
-var application=angular.module('git',[]);
+var app=angular.module('git',["ngRoute"]);
+app.config(function($routeProvider){
+
+    $routeProvider
+      .when("/contactUs",{
+		  templateUrl:"contactus/contactus.html"
+	  });
+	  
+});
