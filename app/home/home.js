8950532421a842e58c72a86217040aee99d39506
Deeptikanta Panda <DPanda@ESTUATE.COM>
2017-03-09 17:31:02 +0530

    session log out

--- app/home/home.js
+++ app/home/home.js
@@ -1,8 +1,7 @@
 app.controller('home',function($scope,$location, $rootScope)
 { if($rootScope.user)
     {
-       alert("welcome to turism.com");
-        
+     console.log("welcm");
     }
  else{
      $location.path("/")
