f9ad4b85bee0dec5b30ba4c9e4201dd45db5a0ae
unknown <VCA@EST-Vibharani.estuate.com>
2017-03-23 17:20:23 +0530

    popup window added

--- app/home/save.js
+++
@@ -1,9 +0,0 @@
-var app=angular.module('git');
-app.controller('aCtrl',function($scope){
-    $scope.click=function(){
-        console.log(" details saved");
-        console.log('user phonenumber:' +$scope.phonenumber) ;
-        console.log('user email:' +$scope.email) ;
-        console.log('user pwd:' +$scope.pwd) ;
-        }
-});
